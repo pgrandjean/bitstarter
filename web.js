ea7668c1cc7bc62cd494234151ab38b8d14d931f
Patrick Grandjean <toto@toto.com>
2013-07-09 20:03:03 +0000

    fix bug

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSyns('index.html');
+  var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
